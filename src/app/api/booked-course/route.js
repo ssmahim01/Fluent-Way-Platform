c8dabbda68cf77c9ba20c980481f754101cd202b
ssmahim01 <mahimzayn666@gmail.com>
2025-02-25 22:26:53 +0600

    Created GET function in api route of booked-course

--- src/app/api/booked-course/route.js
+++ src/app/api/booked-course/route.js
@@ -1,7 +1,23 @@
+import { authOptions } from "@/lib/authOptions";
 import connectMongoDB from "@/lib/connectMongoDB";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export const POST = async(req) => {
+export const GET = async (req) => {
+    const session = await getServerSession(authOptions);
+    
+    if (session) {
+        const email = session?.user?.email
+        // console.log(email);
+        const bookingCollection = connectMongoDB("bookedCourse");
+        const result = await bookingCollection.find({ email }).toArray();
+        return NextResponse.json(result);
+    }
+
+    return NextResponse.json({});
+}
+
+export const POST = async (req) => {
     const bookInfo = await req.json();
     const bookedCourseCollection = connectMongoDB("bookedCourse");
     const storeBookingData = await bookedCourseCollection.insertOne(bookInfo);
